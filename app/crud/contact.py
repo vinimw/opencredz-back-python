57d6fe8ae2e506b7b2d30d07de6da20220ec512b
Vinicius Weber <viniciusweber@MacBook-Pro-de-Vinicius.local>
2025-06-26 02:17:19 -0300

    build: config render build

--- app/crud/contact.py
+++ app/crud/contact.py
@@ -1,6 +1,7 @@
 from typing import List
 from sqlalchemy.orm import Session
 from app.models.contact import Contact
+from app.schemas.contact import ContactCreate
 
 def delete_contact_by_id(db: Session, contact_id: int) -> bool:
     contact = db.query(Contact).filter(Contact.id == contact_id).first()
@@ -12,3 +13,11 @@ def delete_contact_by_id(db: Session, contact_id: int) -> bool:
 
 def get_all_contacts(db: Session) -> List[Contact]:
     return db.query(Contact).order_by(Contact.id.desc()).all()
+
+
+def create_contact(db: Session, contact_in: ContactCreate):
+    contact = Contact(**contact_in.dict())
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+    return contact
