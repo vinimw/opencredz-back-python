950f8339e1428c917571500a6589da3c7296cba5
Vinicius Weber <viniciusweber@MacBook-Pro-de-Vinicius.local>
2025-07-05 00:58:21 -0300

    feat: remove email required

--- app/models/health_plan.py
+++ app/models/health_plan.py
@@ -6,6 +6,6 @@ class HealthPlan(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String)
     phone = Column(String, nullable=False)
     lifes = Column(String, nullable=False)
